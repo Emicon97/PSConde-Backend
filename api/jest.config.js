f0546eefad464ba9f513f7399812811f901115f6
EmiCon97 <derleuchtturm97@gmail.com>
2022-11-29 18:23:31 -0300

    Makefile

--- api/jest.config.js
+++ api/jest.config.js
@@ -4,9 +4,9 @@ const { compilerOptions } = require('./tsconfig.json');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/dist'],
+  roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/dist' }),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
 };
